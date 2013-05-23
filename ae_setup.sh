dfa96e155b9efc9c269fe853641d4dcf2c5b84c8
fabiantheblind <fabiantheblind@googlemail.com>
2013-05-23 08:18:09 +0200

    inital local commit

---
+++ ae_setup.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# create a ae layout for starters
+mkdir aeProj
+mkdir footage
+mkdir footage/audio
+#the ignore folder needs to be added
+#to the "exclude:" of the _config.yml
+mkdir render
+mkdir render/ausspielung
+mkdir render/preview
+mkdir workfiles
+mkdir tryouts
---
+++ gitignore_build.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+# create my personal gitignore
+echo ".DS_Store" > .gitignore
